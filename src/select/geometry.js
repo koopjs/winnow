e92a9ebffb4629e01bcf2870487cb25a6c70ae78
dmfenton <dfenton@esri.com>
2017-05-24 13:48:05 -0400

    add geometry precision option

--- src/select/geometry.js
+++ src/select/geometry.js
@@ -1,8 +1,8 @@
 const compose = require('./compose')
 function createClause (options = {}) {
   const funcs = []
-  if (options.geometryPrecision) funcs.push({ geometryPrecision: options.geometryPrecision })
-  if (options.projection) funcs.push({ project: options.projection })
+  if (options.projection) funcs.push({ project: true })
+  if (options.geometryPrecision) funcs.push({ reducePrecision: true })
   if (options.toEsri) funcs.push('esriGeom')
 
   return funcs.length ? compose(funcs, 'geometry') + ' as geometry' : 'geometry'
