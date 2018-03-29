b7c9b861e90f798f9e54a7a7565d0223b79baf41
Rich Gwozdz <rich.gwozdz@gmail.com>
2018-03-29 07:27:10 -0700

    Keep consistent one-liner style.

--- src/select/index.js
+++ src/select/index.js
@@ -3,7 +3,7 @@ const createGeometryClause = require('./geometry').createClause
 const createFieldsClause = require('./fields').createClause
 
 function createClause (options) {
-  if (options.aggregates) { return aggregates(options.aggregates, options.groupBy, options.esri) }
+  if (options.aggregates) return aggregates(options.aggregates, options.groupBy, options.esri)
 
   var fieldsClause = createFieldsClause(options)
 
