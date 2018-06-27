6a88264c1e0a605cdd3ae30ccf43f9882e8c52f0
Rich Gwozdz <rich.gwozdz@gmail.com>
2018-06-27 11:47:28 -0700

    Rename spatial reference options to avoid conflation with request parameters.

--- src/geometry/project-coordinates.js
+++ src/geometry/project-coordinates.js
@@ -3,13 +3,13 @@ const _ = require('lodash')
 const transformCoordinates = require('./transform-coordinates')
 
 module.exports = function projectCoordinates (coordinates, options = {}) {
-  const inSR = options.inSR || 'EPSG:4326'
-  const outSR = options.outSR || 'EPSG:3857'
-  if (inSR === outSR) return coordinates
+  const sourceSR = options.sourceSR || 'EPSG:4326'
+  const targetSR = options.targetSR || 'EPSG:3857'
+  if (sourceSR === targetSR) return coordinates
 
-  return transformCoordinates(coordinates, { inSR, outSR }, (coordinates, options) => {
+  return transformCoordinates(coordinates, { sourceSR, targetSR }, (coordinates, options) => {
     if (_.isNumber(coordinates[0]) && _.isNumber(coordinates[1])) {
-      return proj4(options.inSR, options.outSR, coordinates)
+      return proj4(options.sourceSR, options.targetSR, coordinates)
     } else {
       return coordinates
     }
