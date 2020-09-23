ca2e5dfe1a777523429778cd3f528513c59b91f0
Haoliang Yu <haoliang.yu@outlook.com>
2020-09-23 10:46:47 -0400

    test it out 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   query: require('./lib/query'),
   prepareQuery: require('./lib/prepare-query'),
+
   querySql: require('./lib/query-sql'),
   prepareSql: require('./lib/prepare-sql')
 }
