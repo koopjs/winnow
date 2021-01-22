966de350f033eb0a390421f064e8d6a0a68e3e8d
rgwozdz <rich.gwozdz@gmail.com>
2021-01-21 16:10:13 -0800

    Add test.

--- test/unit/calculate-class-breaks/filter-and-validate-classification-features.spec.js
+++ test/unit/calculate-class-breaks/filter-and-validate-classification-features.spec.js
@@ -1,7 +1,7 @@
 const test = require('tape')
 const filterAndValidateClassificationValues = require('../../../lib/calculate-class-breaks/filter-and-validate-classification-features')
 
-test('filterAndValidateClassificationValues: invalid std dev interval', spec => {
+test('filterAndValidateClassificationValues: success', spec => {
   const features = [
     { properties: { rain: 0.1 } },
     { properties: { rain: '0.1' } },
@@ -9,6 +9,23 @@ test('filterAndValidateClassificationValues: invalid std dev interval', spec => 
     { properties: { } }
   ]
   const result = filterAndValidateClassificationValues(features, 'rain')
-  spec.deepEquals(result, [5.866775, 16.133225])
+  spec.deepEquals(result, [0.1, 0.1, 0])
+  spec.end()
+})
+
+test('filterAndValidateClassificationValues: should throw error', spec => {
+  const features = [
+    { properties: { rain: 0.1 } },
+    { properties: { rain: 'foo' } },
+    { properties: { rain: 0 } },
+    { properties: { } }
+  ]
+
+  try {
+    filterAndValidateClassificationValues(features, 'rain')
+    spec.fail('should throw error')
+  } catch (error) {
+    spec.equals(error.message, 'Cannot use non-numeric classificationField, rain: "foo"')
+  }
   spec.end()
 })
